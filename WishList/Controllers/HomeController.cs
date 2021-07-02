0f2b4fee52de767a6ce51ec4ca79255c69f8b26d
mohitmt <Mohit.Dharmadhikari@mindtree.com>
2021-07-02 12:23:00 +0000

    Home controller
    Home controller

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -3,6 +3,14 @@ namespace WishList.Controllers
 {
     public class HomeController: Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
 
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
