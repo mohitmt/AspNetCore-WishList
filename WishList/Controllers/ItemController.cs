3577fabad9cbf5425eba70fd6980614115853b5d
mohitmt <Mohit.Dharmadhikari@mindtree.com>
2021-07-02 13:42:29 +0000

    Completed module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,8 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
 namespace WishList.Controllers
 {
-    class ItemController: Controller
+    public class ItemController: Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         
+        public IActionResult Index()
+        {
+            return View(_context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create( Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items[Id]
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return View(_context.Items);
+        }
     }
 }
